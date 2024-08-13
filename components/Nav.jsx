4c2073398d9b3931023b34d8c7900a541080b091
Annelies11 <mmahmudi1103@gmail.com>
2024-08-13 16:44:06 +0700

    create post

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,11 +4,24 @@ import Image from "next/image"
 import { useState, useEffect } from "react"
 import {signIn,signOut,useSession,getProviders} from 'next-auth/react'
 const Nav = () => {
+  const isUserLoggedIn = true;
   return (
     <nav className="flex-between w-full mb-16 pt-3">
         <Link href="/" className="flex gap-2 flex-center">
         <Image src="/assets/images/logo.svg"alt="Promptia" width={30} height={30} className="object-contain"/>
+        <p className="logo_text">Promptia</p>
         </Link>
+        <div className="sm:flex hidden">
+          {isUserLoggedIn ? (
+            <div className="flex gap-3 md:gap-5">
+              <Link href="/create-prompt" className="black_btn">Create Post</Link>
+            </div>
+          ):(
+            <>
+              <button type="button" onClick={signOut} className="outline_btn"></button>
+            </>
+          )}
+        </div>
     </nav>
   )
 }
