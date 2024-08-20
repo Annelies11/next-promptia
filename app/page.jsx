4a3b10fb3d076aa74f654d4c492496e285b02132
Annelies11 <mmahmudi1103@gmail.com>
2024-08-20 20:48:16 +0700

    Feed start

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ const Home = () => {
             <br className="max-md:hidden"/>
             <span className="orange_gradient text-center"> AI-Powered Prompts</span>
             </h1>
-            <p className="desc text-center">Promptopia is an open-source AI Prompting tool for modern world to discover, create and share creative prompts</p>
+            <p className="desc text-center">Promptia is an open-source AI Prompting tool for modern world to discover, create and share creative prompts</p>
             <Feed />
     </section>
   )
