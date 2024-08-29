99d06b77c1e536888f874c5a4b93e20fbc843565
Annelies11 <mmahmudi1103@gmail.com>
2024-08-29 20:25:25 +0700

    finish

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,47 +1,48 @@
-'use client'
+"use client";
 
-import {useState} from 'react'
-import { useSession } from 'next-auth/react'
-import { useRouter } from 'next/navigation'
-import Form from '@components/Form'
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import Form from "@components/Form";
 const CreatePrompt = () => {
   const router = useRouter();
-  const {data: session} = useSession();
-  const [submitting, setSubmitting] = useState(false)
+  const { data: session } = useSession();
+  const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
-    prompt: '',
-    tag: '',
-  })
+    prompt: "",
+    tag: "",
+  });
   const createPrompt = async (e) => {
     e.preventDefault();
     setSubmitting(true);
     try {
-      const response = await fetch('/api/prompt/new',{
-        method: 'POST',
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
         body: JSON.stringify({
           prompt: post.prompt,
           userId: session?.user.id,
-          tag: post.tag
-        })
-      })
-      if(response.ok){
-        router.push('/');
+          tag: post.tag,
+        }),
+      });
+      if (response.ok) {
+        router.push("/");
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     } finally {
       setSubmitting(false);
     }
-  }
+  };
   return (
-    <Form 
-    type="Create"
-    post={post}
-    setPost={setPost}
-    submitting={submitting}
-    handleSubmit={createPrompt}
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+      placeholder="Write your prompt here..."
     />
-  )
-}
+  );
+};
 
-export default CreatePrompt
+export default CreatePrompt;
